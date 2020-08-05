1159c9c6fd49b5880b3c31f067f42e9cc3a0dfbb
SARATH KAUL <kaul.sarath@gmail.com>
2020-08-05 19:00:15 +0530

    Edu Round 92

---
+++ Codeforces/#Edu_92_A.py
@@ -0,0 +1,34 @@
+import math
+import datetime
+import collections
+import statistics
+import itertools
+
+
+def is_prime(num):
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def input_list():
+    ll = list(map(int, input().split(" ")))
+    return ll
+
+def gcd(a,b): 
+    if a == 0: 
+        return b 
+    return gcd(b % a, a) 
+  
+def lcm(a,b): 
+    return (a*b) / gcd(a,b) 
+
+tc = int(input())
+for _ in range(tc):
+    x,y = map(int,input().split())
+    if x*2 > y:
+        print(-1,-1)
+    else:
+        print(x,2*x)
+        
