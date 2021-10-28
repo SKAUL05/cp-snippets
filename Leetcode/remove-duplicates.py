469f6cc15b75cc648d865f70a5c4cb165f82cb9b
Sarath Kaul <kaul.sarath@gmail.com>
2021-10-28 06:08:51 +0000

    LeetCode Changes

--- Leetcode/remove-duplicates.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        a = set(nums)
-        nums[: len(a)] = sorted(a)
-        return len(a)
--- Leetcode/sum-of-unique-elements.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def sumOfUnique(self, nums: List[int]) -> int:
-        l = [x for x in nums if nums.count(x) == 1]
-        return sum(l)
