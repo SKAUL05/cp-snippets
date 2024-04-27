ac65733cc2ab87ed4d7a32eab712b67ed0c50a8c
SKAUL05 <kaul.sarath@gmail.com>
2024-04-27 11:27:30 +0530

    Leetcode Changes

---
+++ Leetcode/product_of_array_except_self.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def productExceptSelf(self, nums):
+        res = []
+
+        acc = 1
+        for n in nums:
+            res.append(acc)
+            acc *= n
+
+        acc = 1
+        for i in reversed(range(len(nums))):
+            res[i] *= acc
+            acc *= nums[i]
+
+        return res
+
+s = Solution()
+print(s.productExceptSelf([1,2,3,4]))
