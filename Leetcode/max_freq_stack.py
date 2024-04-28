8b64fc4d6896d219631c37e11d68f0078ae909f7
SKAUL05 <kaul.sarath@gmail.com>
2024-04-28 10:38:57 +0530

    Leetcode Changes

---
+++ Leetcode/max_freq_stack.py
@@ -0,0 +1,68 @@
+"""
+Design a stack-like data structure to push elements to the stack and pop the most frequent element from the stack.
+
+Implement the FreqStack class:
+
+FreqStack() constructs an empty frequency stack.
+void push(int val) pushes an integer val onto the top of the stack.
+int pop() removes and returns the most frequent element in the stack.
+If there is a tie for the most frequent element, the element closest to the stack's top is removed and returned.
+
+
+Example 1:
+
+Input
+["FreqStack", "push", "push", "push", "push", "push", "push", "pop", "pop", "pop", "pop"]
+[[], [5], [7], [5], [7], [4], [5], [], [], [], []]
+Output
+[null, null, null, null, null, null, null, 5, 7, 5, 4]
+
+Explanation
+FreqStack freqStack = new FreqStack();
+freqStack.push(5); // The stack is [5]
+freqStack.push(7); // The stack is [5,7]
+freqStack.push(5); // The stack is [5,7,5]
+freqStack.push(7); // The stack is [5,7,5,7]
+freqStack.push(4); // The stack is [5,7,5,7,4]
+freqStack.push(5); // The stack is [5,7,5,7,4,5]
+freqStack.pop();   // return 5, as 5 is the most frequent. The stack becomes [5,7,5,7,4].
+freqStack.pop();   // return 7, as 5 and 7 is the most frequent, but 7 is closest to the top. The stack becomes [5,7,5,4].
+freqStack.pop();   // return 5, as 5 is the most frequent. The stack becomes [5,7,4].
+freqStack.pop();   // return 4, as 4, 5 and 7 is the most frequent, but 4 is closest to the top. The stack becomes [5,7].
+"""
+from collections import defaultdict
+class FreqStack(object):
+
+    def __init__(self):
+        self.frequency = defaultdict(int)
+        self.stack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.frequency[val] += 1
+        if self.frequency[val] > len(self.stack):
+            self.stack.append([val])
+        else:
+            self.stack[self.frequency[val]-1].append(val)
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        val = self.stack[-1].pop()
+        if not self.stack[-1]:
+            self.stack.pop()
+        self.frequency[val] -=1
+        return val
+# Your FreqStack object will be instantiated and called as such:
+obj = FreqStack()
+obj.push(5)
+obj.push(7)
+obj.push(5)
+obj.push(7)
+obj.push(4)
+obj.push(5)
+print(obj.pop())
