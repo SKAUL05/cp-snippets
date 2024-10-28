657c0b33b73a4f843f1eeb5543e796bd83466778
Sarath Kaul <kaul.sarath@gmail.com>
2024-10-28 11:47:52 +0530

    Time: 16 ms (100%), Space: 11.6 MB (100%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.py
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.py
@@ -4,15 +4,14 @@ class Solution(object):
         :type word: str
         :rtype: int
         """
-        cnt = 1
-        sums = 0
+        count = 1  
+        consecutive_count = 0
+
         for i in range(1, len(word)):
-            if word[i] == word[i - 1]:
-                sums += 1
+            if word[i] == word[i-1]:
+                consecutive_count += 1
             else:
-                if sums > 0:
-                    cnt += sums
-                    sums = 0
-        if sums > 0:
-            cnt += sums
-        return cnt
+                count += consecutive_count
+                consecutive_count = 0
+
+        return count + consecutive_count
