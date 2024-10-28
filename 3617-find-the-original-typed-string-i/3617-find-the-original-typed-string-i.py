20d629999cc791779a8e761b53c7fd527f99ff69
Sarath Kaul <kaul.sarath@gmail.com>
2024-10-28 11:45:51 +0530

    Time: 16 ms (100%), Space: 11.6 MB (100%) - LeetHub

---
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def possibleStringCount(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        cnt = 1
+        sums = 0
+        for i in range(1, len(word)):
+            if word[i] == word[i-1]:
+                sums+=1
+            else:
+                if sums > 0:
+                    cnt += sums
+                    sums = 0
+        if sums>0:
+            cnt += sums
+        return cnt
